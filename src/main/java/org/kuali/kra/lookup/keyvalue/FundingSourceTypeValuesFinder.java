f543de0c7a1000ffdb58ee5ef21671834268a21c
Adam Kost <akost@eaglecrk.com>
2014-10-24 13:39:29 -0500

    UAR-609 Commiting changes including:  ProtocolReferenceTypeValuesFinder: removed method that was unmodified from base  FundingSourceTypeValuesFinder: Fix for Funding Sources section: Funding Sources panel: Funding Type dropdown  ProtocolReferenceTypeValuesFinderBase: Fix for Additional Information section: Other Identifier panel: Type dropdown

--- src/main/java/org/kuali/kra/lookup/keyvalue/FundingSourceTypeValuesFinder.java
+++ src/main/java/org/kuali/kra/lookup/keyvalue/FundingSourceTypeValuesFinder.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2005-2010 The Kuali Foundation
  * 
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Educational Community License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.opensource.org/licenses/ecl1.php
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.kuali.kra.lookup.keyvalue;
 
@@ -27,21 +24,23 @@ import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 
 public class FundingSourceTypeValuesFinder extends UifKeyValuesFinderBase {
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 2887646485972440099L;
 
 	@Override
 	public List<KeyValue> getKeyValues() {
 		List<KeyValue> keyValues = new ArrayList<KeyValue>();
 		Collection<FundingSourceType> fundingSourceTypes = (Collection<FundingSourceType>) KraServiceLocator
-				.getService(BusinessObjectService.class).findAll(
-						FundingSourceType.class);
-		for (FundingSourceType fundingSourceType : fundingSourceTypes) {
-			if (fundingSourceType.getFundingSourceTypeFlag()) {
-				keyValues.add(new ConcreteKeyValue(fundingSourceType
-						.getFundingSourceTypeCode(), fundingSourceType
-						.getDescription()));
+				.getService( BusinessObjectService.class ).findAllOrderBy( FundingSourceType.class,
+						"description", true );
+		for ( FundingSourceType fundingSourceType : fundingSourceTypes ) {
+			if ( fundingSourceType.getFundingSourceTypeFlag() ) {
+				keyValues.add( new ConcreteKeyValue( fundingSourceType.getFundingSourceTypeCode(), fundingSourceType.getDescription() ) );
 			}
 		}
-		keyValues.add(0, new ConcreteKeyValue("", "select"));
+		keyValues.add( 0, new ConcreteKeyValue( "", "select" ) );
 		return keyValues;
 	}
 
--- src/main/java/org/kuali/kra/protocol/protocol/ProtocolReferenceTypeValuesFinderBase.java
+++ src/main/java/org/kuali/kra/protocol/protocol/ProtocolReferenceTypeValuesFinderBase.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2005-2014 The Kuali Foundation
  * 
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Educational Community License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.opensource.org/licenses/ecl1.php
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.kuali.kra.protocol.protocol;
 
@@ -28,42 +25,33 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-public abstract class ProtocolReferenceTypeValuesFinderBase extends
-		UifKeyValuesFinderBase {
+public abstract class ProtocolReferenceTypeValuesFinderBase extends UifKeyValuesFinderBase {
 	/**
 	 * Comment for <code>serialVersionUID</code>
 	 */
 	private static final long serialVersionUID = -7074168804030309949L;
 
 	/**
-	 * Constructs the list of ProtocolBase Types. Each entry in the list is a
-	 * &lt;key, value&gt; pair, where the "key" is the unique status code and
-	 * the "value" is the textual description that is viewed by a user. The list
-	 * is obtained from the PROTOCOL_TYPE database table via the
-	 * "KeyValueFinderService".
+	 * Constructs the list of ProtocolBase Types. Each entry in the list is a &lt;key, value&gt; pair, where the "key"
+	 * is the unique status code and the "value" is the textual description that is viewed by a user. The list is
+	 * obtained from the PROTOCOL_TYPE database table via the "KeyValueFinderService".
 	 * 
-	 * @return the list of &lt;key, value&gt; pairs of abstract types. The first
-	 *         entry is always &lt;"", "select:"&gt;.
+	 * @return the list of &lt;key, value&gt; pairs of abstract types. The first entry is always &lt;"", "select:"&gt;.
 	 * @see org.kuali.rice.krad.keyvalues.KeyValuesFinder#getKeyValues()
 	 */
+	@SuppressWarnings( "rawtypes" )
 	@Override
 	public List<KeyValue> getKeyValues() {
-		KeyValuesService keyValuesService = (KeyValuesService) KraServiceLocator
-				.getService("keyValuesService");
-		Collection protocolReferenceTypes = keyValuesService.findAllOrderBy(
-				getProtocolReferenceTypeBOClassHook(),
-				"protocolReferenceTypeCode", true);
+		KeyValuesService keyValuesService = (KeyValuesService) KraServiceLocator.getService( "keyValuesService" );
+		Collection protocolReferenceTypes = keyValuesService.findAllOrderBy( getProtocolReferenceTypeBOClassHook(),
+				"description", true );
 		List<KeyValue> keyValues = new ArrayList<KeyValue>();
-		keyValues.add(0,
-				new ConcreteKeyValue(PrefixValuesFinder.getPrefixKey(),
-						PrefixValuesFinder.getDefaultPrefixValue()));
-		for (Iterator iter = protocolReferenceTypes.iterator(); iter.hasNext();) {
-			ProtocolReferenceTypeBase protocolReferenceType = (ProtocolReferenceTypeBase) iter
-					.next();
-			if (protocolReferenceType.isActive()) {
-				keyValues.add(new ConcreteKeyValue(protocolReferenceType
-						.getProtocolReferenceTypeCode().toString(),
-						protocolReferenceType.getDescription()));
+		keyValues.add( 0, new ConcreteKeyValue( PrefixValuesFinder.getPrefixKey(), PrefixValuesFinder.getDefaultPrefixValue() ) );
+		for ( Iterator iter = protocolReferenceTypes.iterator() ; iter.hasNext() ; ) {
+			ProtocolReferenceTypeBase protocolReferenceType = (ProtocolReferenceTypeBase) iter.next();
+			if ( protocolReferenceType.isActive() ) {
+				keyValues.add( new ConcreteKeyValue( protocolReferenceType.getProtocolReferenceTypeCode().toString(),
+						protocolReferenceType.getDescription() ) );
 			}
 		}
 		return keyValues;
