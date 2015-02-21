406492b39c4eb7a7322c8fe3c720d6128ae16b54
Scott G. Skinner <sskinner@email.arizona.edu>
2015-02-20 17:14:47 -0700

    UAR-973 - Adding all files for the UnitAdministratorCreateGlobal and UnitAdministratorCreateGlobal mod.

---
+++ src/main/java/edu/arizona/kra/global/unit/create/UnitAdminCreateGlobalMaintainableImpl.java
@@ -0,0 +1,83 @@
+package edu.arizona.kra.global.unit.create;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kra.bo.UnitAdministrator;
+import org.kuali.rice.kns.maintenance.KualiGlobalMaintainableImpl;
+import org.kuali.rice.krad.bo.GlobalBusinessObjectDetail;
+import org.kuali.rice.krad.bo.PersistableBusinessObject;
+import org.kuali.rice.krad.maintenance.MaintenanceLock;
+
+import edu.arizona.kra.global.DelimiterConstants;
+import edu.arizona.kra.global.unit.UnitAdministratorPrimaryKey;
+
+/**
+ * This class is used by the framework to generate locks
+ * for the UnitAdministrationCreateGlobal class and its
+ * children. These methods are from the Maintainable interface.
+ */
+@SuppressWarnings("deprecation")
+public class UnitAdminCreateGlobalMaintainableImpl extends KualiGlobalMaintainableImpl {
+	private static final long serialVersionUID = -5289542451038756040L;
+	
+	
+	/**
+	 * Build a lock key of the form:
+	 * "UnitAdministarator!!personId^^171293712973::unitAdministratorTypeCode^^16::unitNumber^^2001"
+	 * 
+	 * Do this for every UnitAdministrator in getBusinessObject().getUnitAdministratorDetails();
+	 * 
+	 * @return A list of locks representing each unique UnitAdministrator represented.
+	 */
+	@Override
+	public List<MaintenanceLock> generateMaintenanceLocks() {
+		
+		List<MaintenanceLock> maintenanceLocks = new ArrayList<MaintenanceLock>();
+		UnitAdministratorCreateGlobal unitAdministratorGlobal = (UnitAdministratorCreateGlobal)getBusinessObject();
+		
+        for (GlobalBusinessObjectDetail adminDetail : unitAdministratorGlobal.getUnitAdminTypeAndPersonGlobalDetails()) {
+        	UnitAdminTypeAndPersonGlobalDetail unitAdminGlobalDetail = (UnitAdminTypeAndPersonGlobalDetail)adminDetail;
+
+            MaintenanceLock maintenanceLock = new MaintenanceLock();
+            StringBuffer lockKeyBuffer = new StringBuffer();
+            
+            // Build first half of key: "UnitAdministarator!!principalName^^foo::unitAdministratorTypeCode^^bar::"
+            lockKeyBuffer.append(UnitAdministrator.class.getName() + DelimiterConstants.AFTER_CLASS.getValue());
+            lockKeyBuffer.append(UnitAdministratorPrimaryKey.PERSON_ID.getValue() + DelimiterConstants.AFTER_FIELDNAME);
+            lockKeyBuffer.append(unitAdminGlobalDetail.getPersonId() + DelimiterConstants.AFTER_VALUE);
+            lockKeyBuffer.append(UnitAdministratorPrimaryKey.UNIT_ADMIN_TYPE_CODE.getValue() + DelimiterConstants.AFTER_FIELDNAME);
+            lockKeyBuffer.append(unitAdminGlobalDetail.getUnitAdministratorTypeCode() + DelimiterConstants.AFTER_VALUE);
+            
+            // Build second half of key: "unitNumber::baz"
+            for(GlobalBusinessObjectDetail unitDetail : unitAdministratorGlobal.getUnitGlobalDetails()) {
+            	UnitGlobalDetail unitGlobalDetail = (UnitGlobalDetail)unitDetail;
+
+            	// One principal can have multiple units being added/removed,
+            	// we need a unique key for each tuple: {principalId, unitAdministratorCode, unitNumber}
+            	// so keep first half of key separate.
+            	String keyFirstHalf = lockKeyBuffer.toString();
+            	lockKeyBuffer = new StringBuffer();
+
+            	lockKeyBuffer.append(UnitAdministratorPrimaryKey.UNIT_NUMBER.getValue() + DelimiterConstants.AFTER_FIELDNAME);
+                lockKeyBuffer.append(unitGlobalDetail.getUnitNumber() + DelimiterConstants.AFTER_VALUE);
+                
+                maintenanceLock.setDocumentNumber(unitAdministratorGlobal.getDocumentNumber());
+                maintenanceLock.setLockingRepresentation(keyFirstHalf + lockKeyBuffer.toString());
+
+            }
+
+            maintenanceLock.setDocumentNumber(unitAdministratorGlobal.getDocumentNumber());
+            maintenanceLock.setLockingRepresentation(lockKeyBuffer.toString());
+            maintenanceLocks.add(maintenanceLock);
+        }
+
+		return maintenanceLocks;
+	}
+
+	@Override
+	public Class<? extends PersistableBusinessObject> getPrimaryEditedBusinessObjectClass() {
+		return UnitAdministrator.class;
+	}
+
+}
