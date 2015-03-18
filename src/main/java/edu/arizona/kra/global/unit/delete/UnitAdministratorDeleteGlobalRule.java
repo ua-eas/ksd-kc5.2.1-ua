d7fd6bf73a9a9f9aff7cfb2ad5041c4ea2749a24
Scott G. Skinner <sskinner@email.arizona.edu>
2015-03-17 17:04:28 -0700

    UAR-973 - adding in fix for sub-task UAR-1162. This adds additional validation.

--- src/main/java/edu/arizona/kra/global/unit/delete/UnitAdministratorDeleteGlobalRule.java
+++ src/main/java/edu/arizona/kra/global/unit/delete/UnitAdministratorDeleteGlobalRule.java
@@ -4,12 +4,23 @@ import java.util.Map;
 
 import org.kuali.kra.bo.UnitAdministrator;
 import org.kuali.kra.infrastructure.KeyConstants;
-import org.kuali.kra.rules.KraMaintenanceDocumentRuleBase;
 import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.krad.util.GlobalVariables;
 
+import edu.arizona.kra.global.unit.UnitAdministratorPrimaryKey;
+
 @SuppressWarnings("deprecation")
-public class UnitAdministratorDeleteGlobalRule extends KraMaintenanceDocumentRuleBase {
+public class UnitAdministratorDeleteGlobalRule extends UnitAdministratorGlobalRuleBase {
+
+	/*
+	 * This corresponds to the UnitAdministratorDeleteGlobalMaintenanceDocument,
+	 * the framework will places the error message next to the UI element
+	 * that has this property name.
+	 */
+	private static final String UNIT_ADMIN_DELETE_GLOBAL_DETAIL_PROPERTY_NAME = "unitAdministratorDeleteGlobal";
+
+
+	
 	
 	@Override
 	protected boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
@@ -18,15 +29,28 @@ public class UnitAdministratorDeleteGlobalRule extends KraMaintenanceDocumentRul
 		
         int errorCount = 0;
         for (Map<String, Object> primaryKeyMap : unitAdministratorDeleteGlobal.getAllUnitAdministratorPrimaryKeys()) {
+        		
         		// Check to ensure this record doesn't exist
         		UnitAdministrator unitAdministrator = unitAdministratorDeleteGlobal.getUnitAdministratorByPrimaryKey(primaryKeyMap);        		
         		if(unitAdministrator == null) {
-        			GlobalVariables.getMessageMap().putError("unitAdministratorDeleteGlobal", KeyConstants.ERROR_UNIT_ADMIN_NONEXISTENT, primaryKeyMap.toString());
+        			GlobalVariables.getMessageMap().putError(UNIT_ADMIN_DELETE_GLOBAL_DETAIL_PROPERTY_NAME,
+        					                                 KeyConstants.ERROR_UNIT_ADMIN_NONEXISTENT,
+        					                                 primaryKeyMap.toString());
         			errorCount++;
         		}
+        		
+        		String personId = (String)primaryKeyMap.get(UnitAdministratorPrimaryKey.PERSON_ID.getValue());
+        		errorCount += validatePersonId(personId, UNIT_ADMIN_DELETE_GLOBAL_DETAIL_PROPERTY_NAME);
+        		
+        		String unitAdminTypeCode = (String)primaryKeyMap.get(UnitAdministratorPrimaryKey.UNIT_ADMIN_TYPE_CODE.getValue());
+        		errorCount += validateUnitAdminTypeCode(unitAdminTypeCode, UNIT_ADMIN_DELETE_GLOBAL_DETAIL_PROPERTY_NAME);
+        		
+        		String unitNumber = (String)primaryKeyMap.get(UnitAdministratorPrimaryKey.UNIT_NUMBER.getValue());
+        		errorCount += validateUnitNumber(unitNumber, UNIT_ADMIN_DELETE_GLOBAL_DETAIL_PROPERTY_NAME);
         }
 
 		return errorCount > 0 ? false : true;
 	}
-	
+
+
 }
