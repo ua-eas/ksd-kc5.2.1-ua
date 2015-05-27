f0eb0710571b46d6f986614f272cf1f14a07ee7c
Adam Kost <akost@eaglecrk.com>
2015-05-27 15:12:02 -0500

    UAR-973 UAR-1197 added validation check to fail validation if the UnitAdministratorType does not allow multiple Unit Admins

--- src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -1111,6 +1111,7 @@ public final class KeyConstants {
     public static final String ERROR_UNIT_ADMIN_TYPE_CODE = "error.unit.admin.type.code.nonexistent";
     public static final String ERROR_UNIT_NUMBER = "error.unit.number.nonexistent";
     public static final String ERROR_UNIT_ADMIN_NO_DETAILS = "error.unit.admin.global.no.details";
+	public static final String ERROR_UNIT_ADMIN_MULTIPLE_FOR_TYPE_NOT_ALLOWED = "error.unit.admin.multiple.for.type.not.allowed";
 
     /**
      * private utility class ctor.
