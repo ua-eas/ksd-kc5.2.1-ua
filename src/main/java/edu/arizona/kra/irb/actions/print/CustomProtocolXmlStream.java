f9be784354dd3873675e4604436ac8bdba5c563b
Adam Kost <akost@eaglecrk.com>
2015-07-18 11:28:34 -0500

    UAR-263 additional requirements fulfilled.

--- src/main/java/edu/arizona/kra/irb/actions/print/CustomProtocolXmlStream.java
+++ src/main/java/edu/arizona/kra/irb/actions/print/CustomProtocolXmlStream.java
@@ -1,10 +1,18 @@
 package edu.arizona.kra.irb.actions.print;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.kuali.kra.bo.CoeusModule;
+import org.kuali.kra.bo.CoeusSubModule;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.irb.actions.print.ProtocolXmlStream;
+import org.kuali.kra.protocol.ProtocolBase;
 import org.kuali.kra.protocol.noteattachment.ProtocolAttachmentProtocolBase;
 import org.kuali.kra.questionnaire.answer.Answer;
 import org.kuali.kra.questionnaire.answer.AnswerHeader;
@@ -74,31 +82,54 @@ public class CustomProtocolXmlStream extends ProtocolXmlStream {
     }
 
 	protected void addQuestionnaires( org.kuali.kra.irb.Protocol protocol, Protocol protocolType ) {
-		QuestionnaireAnswerService qas = KraServiceLocator.getService( QuestionnaireAnswerService.class );
-		List<AnswerHeader> questionnaireAnswerHeaders = qas.getAnswerHeadersForProtocol( protocol.getProtocolNumber() );
-		for ( AnswerHeader questionnaireAnswerHeader : questionnaireAnswerHeaders ) {
-			Questionnaires questionnairesType = protocolType.addNewQuestionnaires();
-			for ( Answer questionnaireAnswer : questionnaireAnswerHeader.getAnswers() ) {
-				QuestionnaireQuestions questionnaireQuestionsType = questionnairesType.addNewQuestionnaireQuestions();
-				String questionID = questionnaireAnswer.getQuestion().getQuestionId();
-				String question = questionnaireAnswer.getQuestion().getQuestion();
-				String questionnaireAnswerID = questionnaireAnswer.getId().toString();
-				String answer = questionnaireAnswer.getAnswer();
-				if ( questionID != null ) {
-					questionnaireQuestionsType.setQuestionID( questionID );
-				}
-				if ( question != null ) {
-					questionnaireQuestionsType.setQuestion( question );
-				}
-				if ( questionnaireAnswerID != null ) {
-					questionnaireQuestionsType.setQuestionnaireAnswerID( questionnaireAnswerID );
-				}
-				if ( answer != null ) {
-					questionnaireQuestionsType.setAnswer( answer );
-				}
-			}
+		AnswerHeader questionnaireAnswerHeader = getQuestionnaireAnswerHeader( protocol.getProtocolNumber() );
+		Questionnaires questionnairesType = protocolType.addNewQuestionnaires();
+		for ( Answer questionnaireAnswer : questionnaireAnswerHeader.getAnswers() ) {
+			QuestionnaireQuestions questionnaireQuestionsType = questionnairesType.addNewQuestionnaireQuestions();
+			String questionID = questionnaireAnswer.getQuestion().getQuestionId();
+			String question = questionnaireAnswer.getQuestion().getQuestion();
+			String questionnaireAnswerID = questionnaireAnswer.getId().toString();
+			String answer = questionnaireAnswer.getAnswer();
+			questionnaireQuestionsType.setQuestionID( questionID );
+			questionnaireQuestionsType.setQuestion( question );
+			questionnaireQuestionsType.setQuestionnaireAnswerID( questionnaireAnswerID );
+			questionnaireQuestionsType.setAnswer( answer );
 		}
-
 	}
+
+	private AnswerHeader getQuestionnaireAnswerHeader( String protocolNumber ) {
+		Map<String, Object> fieldValues = new HashMap<String, Object>();
+		fieldValues.put( MODULE_ITEM_CODE, CoeusModule.IRB_MODULE_CODE );
+		fieldValues.put( MODULE_ITEM_KEY, protocolNumber );
+		List<AnswerHeader> answerHeaders = (List<AnswerHeader>) getBusinessObjectService().findMatching( AnswerHeader.class, fieldValues );
+		if ( answerHeaders == null ) {
+			AnswerHeader ah = new AnswerHeader();
+			ah.setAnswerHeaderId( 0L );
+			List<Answer> answers = new ArrayList<Answer>();
+			ah.setAnswers( answers );
+		}
+		Collections.sort( answerHeaders, ANSWER_HEADER_SORT );
+		return answerHeaders.get( answerHeaders.size() - 1 );
+	}
+
+	private static final String MODULE_ITEM_CODE = "moduleItemCode";
+	private static final String MODULE_ITEM_KEY = "moduleItemKey";
+
+	public static final Comparator<AnswerHeader> ANSWER_HEADER_SORT = new Comparator<AnswerHeader>() {
+		private final Integer BEFORE = -1;
+		private final Integer AFTER = 1;
+		private final Integer SAME = 1;
+
+		@Override
+		public int compare( AnswerHeader arg0, AnswerHeader arg1 ) {
+			if ( arg0.getAnswerHeaderId() < arg1.getAnswerHeaderId() ) {
+				return BEFORE;
+			}
+			if ( arg0.getAnswerHeaderId() > arg1.getAnswerHeaderId() ) {
+				return AFTER;
+			}
+			return SAME;
+		}
+	};
 
 }
