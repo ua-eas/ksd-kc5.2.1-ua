badcb9479dea3be307660823870f869551b65f1e
Alex Angeles <aangeles@email.arizona.edu>
2016-06-03 11:22:00 -0700

    UAR-697 Applied patch provided by foundation.

--- src/main/java/org/kuali/kra/irb/actions/grantexception/ProtocolGrantExemptionServiceImpl.java
+++ src/main/java/org/kuali/kra/irb/actions/grantexception/ProtocolGrantExemptionServiceImpl.java
@@ -77,6 +77,7 @@ public class ProtocolGrantExemptionServiceImpl implements ProtocolGrantExemption
         
         protocol.setApprovalDate(actionBean.getApprovalDate());
         protocol.refreshReferenceObject("protocolStatus");
-        documentService.saveDocument(protocol.getProtocolDocument());
+        protocol.getProtocolDocument().getDocumentHeader().getWorkflowDocument().approve(actionBean.getComments());
+
     }
 }
