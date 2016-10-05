ac8801b743ca1979775cc209cff03aa78fcea9d2
Natalia Ibanez <nataliac@email.arizona.edu>
2016-10-04 17:32:55 -0700

    UAR-1685: Simplifying Award lookup by removing TM status docs and additional unnecessary searching and filtering

--- src/main/java/org/kuali/kra/dao/ojb/AwardLookupDaoOjb.java
+++ src/main/java/org/kuali/kra/dao/ojb/AwardLookupDaoOjb.java
@@ -38,70 +38,12 @@ public class AwardLookupDaoOjb extends LookupDaoOjb implements AwardLookupDao {
 	@SuppressWarnings( "rawtypes" )
 	@Override
 	public List<? extends BusinessObject> getAwardSearchResults( Map fieldValues, boolean usePrimaryKeys ) {
-		List<Award> searchResults = (List<Award>) getVersionHistoryLookupDao().getSequenceOwnerSearchResults( Award.class, fieldValues, usePrimaryKeys );
-		List<String> activeAwards = new ArrayList<String>();
-		List<Long> awardIds = new ArrayList<Long>();
+		//only search for 'active' versions of the award, avoiding the post-search filtering
+		fieldValues.put("awardSequenceStatus", VersionStatus.ACTIVE.name());
 
-		for ( Object object : searchResults ) {
-			Award awardSearchBo = (Award) object;
-
-			if ( VersionStatus.ACTIVE.name().equals( awardSearchBo.getAwardSequenceStatus() ) ) {
-				activeAwards.add( awardSearchBo.getAwardNumber() );
-				awardIds.add( awardSearchBo.getAwardId() );
-			}
-			else if ( !activeAwards.contains( awardSearchBo.getAwardNumber() ) && checkAwardHasActiveTnMDocument( awardSearchBo ) ) {
-				activeAwards.add( awardSearchBo.getAwardNumber() );
-				awardIds.add( awardSearchBo.getAwardId() );
-			}
-		}
-
-		if ( awardIds.isEmpty() ) {
-			return new ArrayList<Award>();
-		}
-
-		Criteria awardCr = new Criteria();
-		awardCr.addIn( "awardId", awardIds );
-		List<Award> awardList = (List<Award>) getPersistenceBrokerTemplate().getCollectionByQuery( QueryFactory.newQuery( Award.class, awardCr ) );
-
-		if ( searchResults instanceof CollectionIncomplete ) {
-			awardList = new CollectionIncomplete<Award>( awardList, ( (CollectionIncomplete<Award>) searchResults ).getActualSizeIfTruncated() );
-		}
-
-		return awardList;
+		return getVersionHistoryLookupDao().getSequenceOwnerSearchResults( Award.class, fieldValues, usePrimaryKeys );
 	}
 
-	private boolean checkAwardHasActiveTnMDocument( Award awardSearckBo ) {
-		Map<String, Object> fieldValues = new HashMap<String, Object>();
-		String[] splitAwardNumber = awardSearckBo.getAwardNumber().split( "-" );
-		StringBuilder rootAwardNumberBuilder = new StringBuilder( 12 );
-
-		rootAwardNumberBuilder.append( splitAwardNumber[0] );
-		rootAwardNumberBuilder.append( "-00001" );
-		String rootAwardNumber = rootAwardNumberBuilder.toString();
-
-		fieldValues.put( "rootAwardNumber", rootAwardNumber );
-
-		Criteria tnmCriteria = new Criteria();
-		tnmCriteria.addEqualTo( "rootAwardNumber", rootAwardNumber );
-
-		QueryByCriteria query = new QueryByCriteria( TimeAndMoneyDocument.class, tnmCriteria );
-		query.addOrderBy( "documentNumber", true );
-
-		List<TimeAndMoneyDocument> timeAndMoneyDocuments = (List<TimeAndMoneyDocument>) getPersistenceBrokerTemplate().getCollectionByQuery( query );
-
-		if ( !timeAndMoneyDocuments.isEmpty() ) {
-			TimeAndMoneyDocument timeAndMoneyDocument = timeAndMoneyDocuments.get( 0 );
-			DocumentHeader documentHeader = timeAndMoneyDocument.getDocumentHeader();
-
-			WorkflowDocument tnmWorkFlowDoc = documentHeader == null || !documentHeader.hasWorkflowDocument() ? null : documentHeader.getWorkflowDocument();
-
-			if ( tnmWorkFlowDoc != null && tnmWorkFlowDoc.isFinal() ) {
-				return true;
-			}
-		}
-
-		return false;
-	}
 
 	public VersionHistoryLookupDao getVersionHistoryLookupDao() {
 		return versionHistoryLookupDao;
