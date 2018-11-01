fe0dde3c9f67eb546d98ca84374d61eea12f7d08
nataliac06 <nataliac@email.arizona.edu>
2018-11-01 11:45:27 -0700

    UAR-2691: Simplyfying BO structure for BIGlEntry and UAGlEntry and adding references to the Error Reporting Service

---
+++ src/main/java/edu/arizona/kra/subaward/batch/service/impl/SubawardInvoiceErrorReportServiceImpl.java
@@ -0,0 +1,31 @@
+package edu.arizona.kra.subaward.batch.service.impl;
+
+/**
+ * Created by nataliac on 10/31/18.
+ */
+public class SubawardInvoiceErrorReportServiceImpl {
+
+    /**
+     * Method that saves the occured exception with the given message
+     */
+    public void recordError(String message, Exception e){
+        //TODO UAR-2692 will implement this!
+    }
+
+
+    /**
+     * Method that saves the occured exception with the given message
+     */
+    public void recordError(String message, Exception e, String glEntryID){
+        //TODO UAR-2692 will implement this!
+    }
+
+
+    /**
+     * Method that saves the occured exception with the given message
+     */
+    public void recordError(String message, String glEntryId, String subawardId, String subawardAmtRelId, String invoiceDocNumber){
+        //TODO UAR-2692 will implement this!
+    }
+
+}
