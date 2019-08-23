9e4951e29a7eda1c2d0c50a5a44d57446d880d5d
nataliac06 <nataliac@email.arizona.edu>
2019-08-23 13:51:34 -0700

    UAR-1962: Simplyfing build for UAR ECS Jenkins upgrade

--- src/main/config/xsd/maven.sh
+++ src/main/config/xsd/maven.sh
@@ -1,9 +1,10 @@
-mvn -Dmaven.test.skip=true install  &&     \
-mvn jar:jar  &&                            \
-mvn install:install-file                   \
+mvn -Dmaven.test.skip=true -Dhttps.protocols=TLSv1.2 install -e -X  &&     \
+mvn jar:jar -Dhttps.protocols=TLSv1.2 -e -X &&                            \
+mvn install:install-file -e -X                \
     -Dpackaging=jar                        \
         -DgroupId=org.kuali.kra            \
     -DartifactId=kc-printing-xsds          \
         -Dversion=2.0.0                    \
     -DgeneratePom=true                     \
+        -Dhttps.protocols=TLSv1.2          \
         -Dfile=target/kc-printing-xsds-2.0.0.jar
