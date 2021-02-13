5bf391268c6db11418994d7e7a4c94db1ec42ed0
sskinner <sskinner@email.arizona.edu>
2021-02-13 13:43:32 -0700

    UARSAAS-573 - Broadening scope after discussion with functionals.

--- src/main/resources/edu/arizona/pdf/protocol_numbers_active.sql
+++ src/main/resources/edu/arizona/pdf/protocol_numbers_active.sql
@@ -14,39 +14,4 @@ and protocol_type_code in (
         'Standard', 'Humanitarian Use Device (HUD)'
     )
 )
-and (
-    (protocol_status_code in (
-        select protocol_status_code
-        from protocol_status
-        where description in (
-            'Active - Closed to Enrollment', 'Active - Data Analysis Only',
-            'Active - Open to Enrollment', 'Deferred',
-            'Exempt', 'IRB review not required',
-            'Suspended by IRB')
-        and protocol_status_code in (
-            select protocol_status_code from protocol_submission where submission_status_code in (
-                select submission_status_code from submission_status where description = 'Approved')))
-    )
-    or
-    (protocol_status_code in (
-        select protocol_status_code
-        from protocol_status
-        where description in (
-            'Closed Administratively for lack of response', 'Disapproved',
-            'Terminated by PI')
-        and protocol_status_code in (
-            select protocol_status_code from protocol_submission where submission_status_code in (
-                select submission_status_code from submission_status where description = 'Closed')))
-    )
-    or
-    (protocol_status_code in (
-        select protocol_status_code
-        from protocol_status
-        where description in (
-            'Amendment in Progress', 'Pending/In Progress',
-            'Renewal in Progress', 'Return To PI',
-            'Specific Minor Revisions Required', 'Submitted to IRB',
-            'Substantive Revisions Required', 'Suspended by DSMB')
-        )
-    )
-)
+order by protocol_number
