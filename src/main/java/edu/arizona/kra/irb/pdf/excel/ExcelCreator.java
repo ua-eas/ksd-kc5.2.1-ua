ce3cbf93e06db669836668a31187c8b50f5f041f
sskinner <sskinner@email.arizona.edu>
2021-02-24 16:58:04 -0700

    Getting rid of sftp functionality.

--- src/main/java/edu/arizona/kra/irb/pdf/excel/ExcelCreator.java
+++ src/main/java/edu/arizona/kra/irb/pdf/excel/ExcelCreator.java
@@ -8,7 +8,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
