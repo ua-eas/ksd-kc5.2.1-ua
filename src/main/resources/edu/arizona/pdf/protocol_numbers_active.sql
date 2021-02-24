2e14edff7b88e0006d5da8da5d54cf028b0f0761
sskinner <sskinner@email.arizona.edu>
2021-02-24 15:34:41 -0700

    jreesepsexcel - Correcting subset sql.

--- src/main/resources/edu/arizona/pdf/protocol_numbers_active.sql
+++ src/main/resources/edu/arizona/pdf/protocol_numbers_active.sql
@@ -1,1 +1,4 @@
-select distinct PROTOCOL_NUMBER from protocol where PROTOCOL_NUMBER in ('0700000468','0600000430','0900000065');
+select distinct PROTOCOL_NUMBER from protocol
+where PROTOCOL_NUMBER in ('0700000468','0600000430','0900000065')
+and update_timestamp >= to_date(?, 'yyyy-mm-dd hh24:mi:ss')
+and update_timestamp <= to_date(?, 'yyyy-mm-dd hh24:mi:ss')
