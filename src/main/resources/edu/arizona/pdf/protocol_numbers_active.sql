038ce62b6154b2e99554ccd3fb4619ea010c0b20
sskinner <sskinner@email.arizona.edu>
2021-04-20 14:50:15 -0700

    HURONSAAS-573 - Updating filter per specification.

--- src/main/resources/edu/arizona/pdf/protocol_numbers_active.sql
+++ src/main/resources/edu/arizona/pdf/protocol_numbers_active.sql
@@ -5,5 +5,8 @@ join protocol_status ps on p.protocol_status_code = ps.protocol_status_code
 where p.active = 'Y'
   and pt.description not in ('Business Associate Agreement','COI Management Plan','Data Use Agreement','Site Authorization')
   and ps.description not in ('Abandoned', 'Closed by Investigator', 'Deleted', 'Not Human Subjects Research',
-                             'Recalled in Routing', 'Suspended by DSMB', 'Suspended by Investigator', 'Withdrawn')
+                             'Recalled in Routing', 'Suspended by DSMB', 'Suspended by Investigator', 'Withdrawn',
+                             'Amendment Incorporated into Protocol','Amendment in Progress','Pending/In Progress',
+                             'Renewal Incorporated into Protocol','Renewal in Progress','Return To PI',
+                             'Specific Minor Revisions Required','Submitted to IRB','Substantive Revisions Required')
 order by protocol_number
